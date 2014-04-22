65e5fa2f778366d1e19ccca323f4649938f9f93f
Samuel Kim <samuelkim@college.harvard.edu>
2014-04-22 19:54:29 -0400

    implemented Guassian Mixture Models

--- final_project_code_mac/classifyCapsule.py
+++ final_project_code_mac/classifyCapsule.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sklearn.cluster import KMeans
+from sklearn.mixture import GMM
 import observedState as state
 
 def isGood(sample):
@@ -14,33 +15,55 @@ def isGood(sample):
 
 def k_means(sample, goodSample=state.getGoodCapsuleExamples(),
             data=None, train=True, plot=False):
-    if train==true:
+    if train==True:
         n_clusters = 3
         est = KMeans(n_clusters)
         est.fit(data)
         centers = est.cluster_centers_
-        pickle(centers, 'capsule_k_means')
+        pickle(est, 'data/capsule_k_means')
     else:
-        centers = unpickle('capsule_k_means')
+        est = unpickle('data/capsule_k_means')
 
     numMatch = 0.0
     numGood = goodSample.shape[0]
 
-    sampleLabel = clusterLabel(centers, sample)
+    #sampleLabel = clusterLabel(centers, sample)
+    sampleLabel = est.predict(sample)
     for i in range(numGood):
-        if clusterLabel(centers,goodSample[i]) == sampleLabel:
+        if est.predict(goodSample[i]) == sampleLabel:
             numMatch += 1
 
     return float(numMatch) / numGood
 
-def clusterLabel(centers, sample):
+def gaussMixture(sample, goodSample=state.getGoodCapsuleExamples(),
+                 data=None, train=True):
+    if train==True:
+        n_classes = 3
+        covar_type = 'full'
+        classifier = GMM(n_components=n_classes, covariance_type=covar_type)
+        classifier.fit(data)
+        pickle(classifier, 'data/capsule_gauss')
+    else:
+        classifier = unpickle('data/capsule_gauss')
+
+    numMatch = 0.0
+    numGood = goodSample.shape[0]
+
+    sampleLabel = classifier.predict(sample)
+    for i in range(numGood):
+        if classifier.predict(goodSample[i]) == sampleLabel:
+            numMatch += 1
+
+    return float(numMatch) / numGood
+
+"""def clusterLabel(centers, sample):
     dists = np.zeros(centers.shape[0])
     for i in range(centers.shape[0]):
         dists[i] = dist(sample, centers)
     return argmin
 
 def dist(pt1, pt2):
-    return np.linalg.norm(pt1 - pt2)
+    return np.linalg.norm(pt1 - pt2)"""
 
 def unpickle(file):
     """Loads and returns a pickled data structure in the given `file` name
