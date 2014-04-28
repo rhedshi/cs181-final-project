f60526b1f8bce3bd06f4a1959f3143e0226bd366
rhedshi <rhedshi@Rheds-MacBook-Pro.local>
2014-04-28 18:19:42 -0400

    latent class and score

--- final_project_code_mac/data/classification_ghosts.py
+++ final_project_code_mac/data/classification_ghosts.py
@@ -15,12 +15,30 @@ ghost_latent_class = ghost_data[:,1]
 ghost_score = ghost_data[:,2]
 ghost_feature_vector = ghost_data[:,3:]
 
-print np.shape(ghost_feature_vector)
+# print np.shape(ghost_feature_vector)
 
-methods = [OneVsRestClassifier(LinearSVC()), OneVsOneClassifier(LinearSVC()), linear_model.LogisticRegression()]
+# ghost_class_0 = ghost_feature_vector[0==ghost_data[:,1]]
+# ghost_class_1 = ghost_feature_vector[1==ghost_data[:,1]]
+# ghost_class_2 = ghost_feature_vector[2==ghost_data[:,1]]
+# ghost_class_3 = ghost_feature_vector[3==ghost_data[:,1]]
+# ghost_class_5 = ghost_feature_vector[5==ghost_data[:,1]]
+
+ghost_class_feature_vector = [ghost_feature_vector[x==ghost_data[:,1]] for x in range(4)]
+
+# ghost_score_0 = ghost_score[0==ghost_data[:,1]]
+# ghost_score_1 = ghost_score[1==ghost_data[:,1]]
+# ghost_score_2 = ghost_score[2==ghost_data[:,1]]
+# ghost_score_3 = ghost_score[3==ghost_data[:,1]]
+# ghost_score_5 = ghost_score[5==ghost_data[:,1]]
+
+ghost_class_score = [ghost_score[x==ghost_data[:,1]] for x in range(4)]
 
 k = 10
 cv_err = []
+
+# Latent Class Ghost Classifier
+'''
+methods = [OneVsRestClassifier(LinearSVC()), OneVsOneClassifier(LinearSVC()), linear_model.LogisticRegression()]
 
 X = ghost_feature_vector
 y = ghost_latent_class
@@ -46,3 +64,39 @@ for method in methods:
 	print
 	print "Avg. Err: %f" % cv_err_mean
 	print "Std. Err: %f" % cv_err_std
+
+OneVsOne = OneVsOneClassifier(LinearSVC()).fit(X,y)
+pickle(OneVsOne,'ghost_latent_class_classifier.pkl')
+'''
+
+# Latent Class Conditional Score Regression
+
+for i in range(4):
+	print "Linear Regression on Scores for Latent Class " + str(i)
+	print "k-fold cross-validation with %d folds" % k
+
+	X = ghost_class_feature_vector[i]
+	y = ghost_class_score[i]
+
+	kf = cross_validation.KFold(len(y),n_folds=k,shuffle=True)
+
+	for train,tests in kf:
+
+	    X_train, y_train, X_test, y_test = X[train], y[train], X[tests], y[tests]
+
+	    result = linear_model.BayesianRidge().fit(X_train,y_train)
+	    preds = result.predict(X_test)
+
+	    cv_err.append(result.score(X_test,y_test))
+	    print "Err on withheld data: %f" % cv_err[-1]
+
+	# calculate mean, std. across folds
+	cv_err_mean, cv_err_std = np.mean(cv_err), np.std(cv_err)
+
+	print
+	print "Avg. Err: %f" % cv_err_mean
+	print "Std. Err: %f" % cv_err_std
+
+	model = linear_model.BayesianRidge().fit(X,y)
+	print model.score(X,y)
+	pickle(model,'ghost_score_' + str(i) + '.pkl')
