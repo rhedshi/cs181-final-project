4666d0f411e9cfecdb4cad900d7c9f168c082909
Casey Grun <casey.grun@gmail.com>
2014-05-04 01:18:33 -0400

    Added code for evaluation and visualization

---
+++ final_project_code_mac/evaluate.py
@@ -0,0 +1,21 @@
+
+import visualize
+
+
+pattern = "python pacman.py -T SrcTeam -p %s -m 1000 -q"
+values = [[
+	"GoodBadCapsuleDistanceAgent",
+	"LocalNeighborhoodAgent",
+	"GhostPositionAgent",
+
+	"SafeAgent",
+	"BadGhostAgent",
+	"CapsuleAgent",
+	"GhostAgent",
+	"NearestBadGhostAgent"
+]]
+
+visualize.savePlots("plots/agents-trajectories.pdf",[visualize.plotScores(*visualize.compareScores(pattern, *values), fmt="%s")])
+
+visualize.savePlots("plots/agents-finals.pdf",[visualize.plotFinalScores(*visualize.compareFinalScores(pattern, *values, n=10), fmt="%s")])
+
