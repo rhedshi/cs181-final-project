4e425ce9d8870ff497f18302c166b72b73fe302a
Casey Grun <casey.grun@gmail.com>
2014-05-04 20:26:14 -0400

    Training code for Q-learning

--- final_project_code_mac/train.py
+++ final_project_code_mac/train.py
@@ -8,4 +8,4 @@ agentName = sys.argv[1]
 repeats = 50
 pattern = "python pacman.py -T SrcTeam -p %s -m 1000 -q"
 values = [[	agentName ] * repeats]
-visualize.savePlots("plots/train-%s.pdf" % agentName,[visualize.plotFinalScores(*visualize.compareFinalScores(pattern, *values, n=1), fmt="%s")])
+visualize.savePlots("plots/train-%s.pdf" % agentName,[visualize.plotFinalScores(*visualize.compareFinalScores(pattern, *values, n=5), fmt="%s")])
